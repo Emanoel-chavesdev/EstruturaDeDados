cf618a80d0d6b4f4daf99e7862ec4d2d7b037d53
Emanoel-chavesdev <111396501+Emanoel-chavesdev@users.noreply.github.com>
2022-08-23 20:23:18 -0300

    Update Questao01.java

--- Atividade01/Questao01.java
+++ Atividade01/Questao01.java
@@ -14,20 +14,20 @@ public class Questao01 {
 
 	    System.out.println(Vetor []);
 }
-	for(int i=0;i<10;i++){
-	System.out.printf("\nResultado do Vetor",i+1);
-				vetor[i]= entrada.nextInt();
+	     for(int i=0;i<10;i++){
+	     System.out.printf("\nResultado do Vetor",i+1);
+	     vetor[i]= entrada.nextInt();
 				}
-	int num;
+	      int num;
 		System.out.println("Digite um valor a ser localizado: ");
-        num = in.nextInt();
-        System.out.println();
+                num = in.nextInt();
+               System.out.println();
 	        
 	    for(int i=0;i<vetor.length;i++){
-	            // se existir no vetor o número digitado
+	            
 	         if(vetor[i] == num){
 	            System.out.println("A posição do numero pedido eh " + i);
-	            // se não existir...
+	            
 	            } else if(i != vetor.length){
 	                System.out.println("Número não existe no vetor");
 	            }
