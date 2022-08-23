051c35c421f8ecd38ca42d366434a3047dfb31e2
Emanoel-chavesdev <111396501+Emanoel-chavesdev@users.noreply.github.com>
2022-08-23 20:08:11 -0300

    Update Questao01.java

--- Atividade01/Questao01.java
+++ Atividade01/Questao01.java
@@ -15,4 +15,20 @@ public class Questao01 {
 	System.out.printf("\nResultado do Vetor",i+1);
 				vetor[i]= entrada.nextInt();
 				}
+	int num;
+		System.out.println("Digite um valor a ser localizado: ");
+        num = in.nextInt();
+        System.out.println();
+	        
+	    for(int i=0;i<vetor.length;i++){
+	            // se existir no vetor o número digitado
+	         if(vetor[i] == num){
+	            System.out.println("A posição do numero pedido eh " + i);
+	            // se não existir...
+	            } else if(i != vetor.length){
+	                System.out.println("Número não encontrado no vetor");
+	            }
+		}
+
+	}
 }
