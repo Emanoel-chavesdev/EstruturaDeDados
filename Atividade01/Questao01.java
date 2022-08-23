4e3e8fdceff93f8ade45ade5d973400823aa8b10
Emanoel-chavesdev <111396501+Emanoel-chavesdev@users.noreply.github.com>
2022-08-23 16:03:27 -0300

    Add files via upload
    Questão 1, Atividade 1

---
+++ Atividade01/Questao01.java
@@ -0,0 +1,14 @@
+package questao01;
+
+import java.util.Random;
+import java.util.Vector;
+
+public class Questao01 {
+
+	public static void main(String[] args) {  
+	    Random numaleatorio = new Random();
+	    int[] vetor = new int[10];
+
+	    System.out.println(Vector []);
+}
+}
