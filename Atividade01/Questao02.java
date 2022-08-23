f6921558a0d4037102ee673dca496a04d6f2a9f9
Emanoel-chavesdev <111396501+Emanoel-chavesdev@users.noreply.github.com>
2022-08-23 20:04:35 -0300

    Update Questao02.java

--- Atividade01/Questao02.java
+++ Atividade01/Questao02.java
@@ -14,6 +14,25 @@ public class Questao02 {
 	impar+=2;
 	}
 	System.out.printf("\n");
+		
+		}
+			
+			System.out.println("Maior valor: " + maiorValor);
+			System.out.println("Menor valor: " + menorValor);
+			
+		}
+		
+		return m;
+	}
+	
+	public static void mostrar(int [][]matriz) {
+		for (int linha = 0; linha < matriz.length; linha++ ) {
+			for (int coluna = 0; coluna < matriz[0].length; coluna++) {
+				System.out.print(matriz[linha][coluna] + "	" );
+			}
+			System.out.println();
+		}
+		
 	}
 	}
 	}
