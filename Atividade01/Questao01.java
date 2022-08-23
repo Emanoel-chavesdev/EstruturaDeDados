cfab29cce552cb70917634a116b579194d3d308b
Emanoel-chavesdev <111396501+Emanoel-chavesdev@users.noreply.github.com>
2022-08-23 20:13:52 -0300

    Update Questao01.java

--- Atividade01/Questao01.java
+++ Atividade01/Questao01.java
@@ -2,14 +2,17 @@ package questao01;
 
 import java.util.Random;
 import java.util.Vector;
+import java.util.Scanner;
 
 public class Questao01 {
 
 	public static void main(String[] args) {  
 	    Random numaleatorio = new Random();
+	    
+	    Scanner scan = new Scanner(System.in);
 	    int[] vetor = new int[10];
 
-	    System.out.println(Vector []);
+	    System.out.println(Vetor []);
 }
 	for(int i=0;i<10;i++){
 	System.out.printf("\nResultado do Vetor",i+1);
